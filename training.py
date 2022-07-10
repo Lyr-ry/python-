88846dd9e5ba34257be81ced1e8c5f5d0262b14a
Lyr-ry <108952664+Lyr-ry@users.noreply.github.com>
2022-07-10 11:08:59 +0800

    Update training.py

--- training.py
+++ training.py
@@ -5,7 +5,6 @@ from loss import*
 from network import*
 import random
 import csv
-import numpy as np
 
 random.seed(1111)
 
@@ -19,35 +18,44 @@ with open('/data/liyiran666/brain/brain/brain5096_train.csv','r') as fp:
         temp_mat1 = np.r_[temp_mat1,temp]
         label1 = label1.extend(temp[-1])
         
-temp_mat2 = np.array([])
-label2 = []
-with open('/data/liyiran666/brain/brain/brain3920_train.csv','r') as fp:
+# temp_mat2 = np.array([])
+# label2 = []
+# with open('/data/liyiran666/brain/brain/brain3920_train.csv','r') as fp:
+#     reader=csv.reader(fp)
+#     for x in reader:
+#         temp = np.array(x[0:-1])
+#         temp_mat1 = np.r_[temp_mat2,temp]
+#         label2 = label2.extend(temp[-1])
+        
+# temp_mat3 = np.array([])
+# label3 = []
+# with open('/data/liyiran666/brain/brain/brain2904_train.csv','r') as fp:
+#     reader=csv.reader(fp)
+#     for x in reader:
+#         temp = np.array(x[0:-1])
+#         temp_mat3 = np.r_[temp_mat3,temp]
+#         label3 = label3.extend(temp[-1])
+        
+# temp_mat4 = np.array([])
+# label4 = []
+# with open('/data/liyiran666/brain/brain/brain1705_train.csv','r') as fp:
+#     reader=csv.reader(fp)
+#     for x in reader:
+#         temp = np.array(x[0:-1])
+#         temp_mat4 = np.r_[temp_mat4,temp]
+#         label4 = label4.extend(temp[-1])
+
+temp_mat_t1 = np.array([])
+label_t1 = []
+with open('/data/liyiran666/brain/brain/brain5096_test.csv','r') as fp:
     reader=csv.reader(fp)
     for x in reader:
         temp = np.array(x[0:-1])
-        temp_mat1 = np.r_[temp_mat2,temp]
-        label2 = label2.extend(temp[-1])
+        temp_mat_t1 = np.r_[temp_mat_t1,temp]
+        label_t1 = label_t1.extend(temp[-1])
         
-temp_mat3 = np.array([])
-label3 = []
-with open('/data/liyiran666/brain/brain/brain2904_train.csv','r') as fp:
-    reader=csv.reader(fp)
-    for x in reader:
-        temp = np.array(x[0:-1])
-        temp_mat3 = np.r_[temp_mat3,temp]
-        label3 = label3.extend(temp[-1])
-        
-temp_mat4 = np.array([])
-label4 = []
-with open('/data/liyiran666/brain/brain/brain1705_train.csv','r') as fp:
-    reader=csv.reader(fp)
-    for x in reader:
-        temp = np.array(x[0:-1])
-        temp_mat4 = np.r_[temp_mat4,temp]
-        label4 = label4.extend(temp[-1])
-        
-rna_mats = [temp_mat1,temp_mat2,temp_mat3,temp_mat4]
-labels = [label1,label2,label3,label4]
+rna_mats = [temp_mat1,temp_mat_t1]
+labels = [label1,label_t1]
 
 solver = Solver(rna_mats=rna_mats, labels=labels)
 
