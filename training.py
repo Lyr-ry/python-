62d683a87b766536542586b783cd634e5c470e82
Lyr-ry <108952664+Lyr-ry@users.noreply.github.com>
2022-07-11 17:10:13 +0800

    Add files via upload

--- training.py
+++ training.py
@@ -5,54 +5,23 @@ from loss import*
 from network import*
 import random
 import csv
+import matplotlib.pyplot as plt
 
 random.seed(1111)
-
-# import module related to tensionboard for generate working journal
-from torch.utils.tensorboard import SummaryWriter
-# creat working journal and store at assigned path
-writer = SummaryWriter('/output/logs')
 
 #data reading
 temp_mat1 = np.array([])
 label1 = []
-with open('/data/liyiran666/brain/brain/brain5096_train.csv','r') as fp:
+with open('/home/qukun/dingyiemail/lyr/brain/brain5096_train.csv','r') as fp:
     reader=csv.reader(fp)
     for x in reader:
         temp = np.array(x[0:-1])
         temp_mat1 = np.r_[temp_mat1,temp]
         label1.extend(temp[-1])
         
-# temp_mat2 = np.array([])
-# label2 = []
-# with open('/data/liyiran666/brain/brain/brain3920_train.csv','r') as fp:
-#     reader=csv.reader(fp)
-#     for x in reader:
-#         temp = np.array(x[0:-1])
-#         temp_mat1 = np.r_[temp_mat2,temp]
-#         label2.extend(temp[-1])
-        
-# temp_mat3 = np.array([])
-# label3 = []
-# with open('/data/liyiran666/brain/brain/brain2904_train.csv','r') as fp:
-#     reader=csv.reader(fp)
-#     for x in reader:
-#         temp = np.array(x[0:-1])
-#         temp_mat3 = np.r_[temp_mat3,temp]
-#         label3.extend(temp[-1])
-        
-# temp_mat4 = np.array([])
-# label4 = []
-# with open('/data/liyiran666/brain/brain/brain1705_train.csv','r') as fp:
-#     reader=csv.reader(fp)
-#     for x in reader:
-#         temp = np.array(x[0:-1])
-#         temp_mat4 = np.r_[temp_mat4,temp]
-#         label4.extend(temp[-1])
-
 temp_mat_t1 = np.array([])
 label_t1 = []
-with open('/data/liyiran666/brain/brain/brain5096_test.csv','r') as fp:
+with open('/home/qukun/dingyiemail/lyr/brain/brain5096_test.csv','r') as fp:
     reader=csv.reader(fp)
     for x in reader:
         temp = np.array(x[0:-1])
@@ -64,13 +33,33 @@ labels = [label1,label_t1]
 
 solver = Solver(rna_mats=rna_mats, labels=labels)
 
+acc_l = []
+loss_l = []
+x_l = []
+
 #train scMRA model for 200 epochs
-for t in range(200):
+for t in range(30):
     print('Epoch: ', t)
     num = solver.train_gcn_adapt(t)
-    best_acc = solver.test(t)
-    #update the Accuracy of train and validate
-    writer.add_scalar('best_acc', best_acc, t)
+    best_acc,loss = solver.test(t)
+    #update the Loss and Accuracy of train and validate
+    acc_l.extend(best_acc)
+    loss_l.extend(loss)
+    x_l.extend(t)
 
+#display the curves of best_accuracy and loss
+plt.figure(1)
+plt.scatter(x_l,acc_l,label='best_acc')
+plt.xlabel("epoch")
+plt.ylabel("accuracy")
+plt.legend(loc="best")
+plt.savefig("/home/qukun/dingyiemail/lyr/output/best_accuracy.png")
+plt.figure(2)
+plt.scatter(x_l,loss_l,label='loss')
+plt.xlabel("epoch")
+plt.ylabel("loss")
+plt.legend(loc="best")
+plt.savefig("/home/qukun/dingyiemail/lyr/output/loss.png")
+    
 ARI = np.around(adjusted_rand_score(np.array(best_acc[1]),np.array(best_acc[0])),5)
 AMI = np.around(adjusted_mutual_info_score(np.array(best_acc[1]),np.array(best_acc[0])),5)
